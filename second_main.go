992385b6cb271a3e86eacc9125afb2432c3b2a92
David Santacruz R <axelsantacruzr@gmail.com>
2024-11-15 19:14:06 -0500

    fix: changed second main to test saving, and retrieval of keys.

--- second_main.go
+++ second_main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	client := redis.Client{}
 	client.New("127.0.0.1", 6379)
-	client.SetValue("juan", "Remoya")
-	var value = client.GetKey("juan")
-	fmt.Println("Received from server:", value)
+	client.SetValue("juan", "moya")
+	client.SetValue("david", "santacruz")
+	var customer1 = client.GetKey("juan")
+	var customer2 = client.GetKey("david")
+	fmt.Println("Received from server:", customer1)
+	fmt.Println("Received from server:", customer2)
 }
